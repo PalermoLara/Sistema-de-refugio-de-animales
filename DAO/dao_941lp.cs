1ac7e44ac4894d8064e47d65ddb6f1790fb12a15
PalermoLara <palermo.lara22@gmail.com>
2025-04-27 21:51:48 -0300

    cambie la dao

--- DAO/dao_941lp.cs
+++ DAO/dao_941lp.cs
@@ -10,66 +10,92 @@ namespace DAO
 {
     public class dao_941lp 
     {
-        DataTable dt_941lp;
-        SqlConnection connection_941lp;
-        SqlCommand cmd_941lp;
+        private readonly string connectionString_941lp;
 
         public dao_941lp()
         {
-            connection_941lp = new SqlConnection("Data Source=.;Initial Catalog=sistAdopcion941lp;Integrated Security=True;");
-            connection_941lp.Open();
-            cmd_941lp = new SqlCommand("", connection_941lp);
+            connectionString_941lp = "Data Source=.;Initial Catalog=sistAdopcion941lp;Integrated Security=True;";
         }
+
+        //public DataTable RetornarTabla_941lp(string tabla_941lp, string query_941lp, Dictionary<string, object> parametros_941lp = null)
+        //{
+        //    dt_941lp = new DataTable(tabla_941lp);
+        //    AsignarParametros_941lp(query_941lp, parametros_941lp);
+        //    using (SqlDataReader dr_941lp = cmd_941lp.ExecuteReader())
+        //    {
+        //        dt_941lp.Load(dr_941lp);
+        //    }
+        //    dt_941lp.PrimaryKey = new DataColumn[] { dt_941lp.Columns[0] };
+        //    return dt_941lp;
+        //}
+
+        //public void Query_941lp(string query_941lp, Dictionary<string, object> parametros_941lp = null)
+        //{
+        //    AsignarParametros_941lp(query_941lp, parametros_941lp);
+        //    cmd_941lp.ExecuteNonQuery();
+        //}
+
+        //private void AsignarParametros_941lp(string query_941lp, Dictionary<string, object> parametros_941lp)
+        //{
+        //    cmd_941lp.CommandText = query_941lp;
+        //    cmd_941lp.Parameters.Clear();
+        //    if (parametros_941lp != null)
+        //    {
+        //        foreach (KeyValuePair<string, object> parametro in parametros_941lp)
+        //        {
+        //            cmd_941lp.Parameters.AddWithValue(parametro.Key, parametro.Value);
+        //        }
+        //    }
+
+
+        //}
 
         public DataTable RetornarTabla_941lp(string tabla_941lp, string query_941lp, Dictionary<string, object> parametros_941lp = null)
         {
-            dt_941lp = new DataTable(tabla_941lp);
-            AsignarParametros_941lp(query_941lp, parametros_941lp);
-            using (SqlDataReader dr_941lp = cmd_941lp.ExecuteReader())
+            DataTable dt = new DataTable(tabla_941lp);
+
+            using (SqlConnection connection = new SqlConnection(connectionString_941lp))
             {
-                dt_941lp.Load(dr_941lp);
+                using (SqlCommand cmd = new SqlCommand(query_941lp, connection))
+                {
+                    AsignarParametros_941lp(cmd, parametros_941lp);
+                    connection.Open();
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        dt.Load(dr);
+                    }
+                }
             }
-            dt_941lp.PrimaryKey = new DataColumn[] { dt_941lp.Columns[0] };
-            return dt_941lp;
+
+            if (dt.Columns.Count > 0)
+                dt.PrimaryKey = new DataColumn[] { dt.Columns[0] };
+
+            return dt;
         }
 
         public void Query_941lp(string query_941lp, Dictionary<string, object> parametros_941lp = null)
         {
-            AsignarParametros_941lp(query_941lp, parametros_941lp);
-            cmd_941lp.ExecuteNonQuery();
-        }
-
-        private void AsignarParametros_941lp(string query_941lp, Dictionary<string, object> parametros_941lp)
-        {
-            cmd_941lp.CommandText = query_941lp;
-            cmd_941lp.Parameters.Clear();
-            if (parametros_941lp != null)
+            using (SqlConnection connection = new SqlConnection(connectionString_941lp))
             {
-                foreach (KeyValuePair<string, object> parametro in parametros_941lp)
+                using (SqlCommand cmd = new SqlCommand(query_941lp, connection))
                 {
-                    cmd_941lp.Parameters.AddWithValue(parametro.Key, parametro.Value);
+                    AsignarParametros_941lp(cmd, parametros_941lp);
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
                 }
             }
         }
 
-        public void Abrir_941lp()
+        private void AsignarParametros_941lp(SqlCommand cmd, Dictionary<string, object> parametros_941lp)
         {
-            connection_941lp.Open();
-        }
-
-        public void Cerrar_941lp()
-        {
-            connection_941lp.Close();
-        }
-
-        public bool Estados_941lp()
-        {
-            bool abierto_941lp = false;
-            if (connection_941lp.State == ConnectionState.Open)
+            cmd.Parameters.Clear();
+            if (parametros_941lp != null)
             {
-                abierto_941lp = true;
+                foreach (var parametro in parametros_941lp)
+                {
+                    cmd.Parameters.AddWithValue(parametro.Key, parametro.Value ?? DBNull.Value);
+                }
             }
-            return abierto_941lp;
         }
     }
 }
